32d855f82aac10c1cb28ded78f3dea1084051964
jwpf100 <jwpf100@gmail.com>
2021-05-05 22:05:00 +0100

    style(currentweatherdisplay): styled current weather display

--- src/components/CurrentForecastDisplay/CurrentForecastDisplay.js
+++ src/components/CurrentForecastDisplay/CurrentForecastDisplay.js
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { weatherIconLink } from '../../utils/api'
 import { addToSearchList } from '../../utils/data'
+import CurrentForecastHeader from '../CurrentForecastHeader'
+import CurrentForecastMain from '../CurrentForecastMain'
 
 const CurrentForecastDisplay = ({
   currentWeatherData,
@@ -17,39 +19,12 @@ const CurrentForecastDisplay = ({
       {Object.keys(currentWeatherData).length === 0 ? (
         <div className="container">Please search for a location...</div>
       ) : (
-        <div>
-          <div className="container border d-flex">
-            <div className="offset-3 col-6 text-center">
-              <h2 className="d-inline-block">{`${currentWeatherData.data[0].city_name} `}</h2>
-              <h4 className="d-inline-block">{`(${currentWeatherData.data[0].country_code})`}</h4>
-              <p>{`(Lat: ${currentWeatherData.data[0].lat} Lon: ${currentWeatherData.data[0].lon})`}</p>
-            </div>
-            <div className="col-3 d-flex justify-content-center align-items-center">
-              <button
-                type="button"
-                className="btn btn-primary m-2"
-                onClick={handleSaveSearch}
-              >
-                Save
-              </button>
-            </div>
-          </div>
-          <div className="container border text-center">
-            <h4>Current Weather</h4>
-            <img
-              src={`${weatherIconLink}${currentWeatherData.data[0].weather.icon}.png`}
-              alt={currentWeatherData.data[0].weather.description}
-            />
-            <p>{`${currentWeatherData.data[0].weather.description}`}</p>
-            <div className="d-flex justify-content-between">
-              <p className="d-inline-block">{`Sunrise: ${currentWeatherData.data[0].sunrise} Sunset: ${currentWeatherData.data[0].sunset}`}</p>
-              <p className="d-inline-block">{`Temperature: ${currentWeatherData.data[0].temp}`}</p>
-            </div>
-            <div className="d-flex justify-content-between">
-              <p>{`Windspeed: ${currentWeatherData.data[0].wind_spd} Direction: ${currentWeatherData.data[0].wind_dir}`}</p>
-              <p>{`Precipitation: ${currentWeatherData.data[0].precip}`}</p>
-            </div>
-          </div>
+        <div className="container">
+          <CurrentForecastHeader
+            currentWeatherData={currentWeatherData}
+            onClick={handleSaveSearch}
+          />
+          <CurrentForecastMain currentWeatherData={currentWeatherData} />
         </div>
       )}
     </>
---
+++ src/components/CurrentForecastMain/CurrentForecastMain.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { weatherIconLink } from '../../utils/api'
+import { degToCompass } from '../../utils/maths'
+
+const CurrentForecastMain = ({ currentWeatherData, className }) => {
+  const weatherIcon = currentWeatherData.data[0].weather.icon
+  const weatherDescription = currentWeatherData.data[0].weather.description
+  const { sunset, sunrise, precip } = currentWeatherData.data[0]
+  const temperature = currentWeatherData.data[0].temp.toFixed()
+  const windSpd = currentWeatherData.data[0].wind_spd
+  const windDir = degToCompass(currentWeatherData.data[0].wind_dir)
+
+  return (
+    <div className={['', className].join(' ')}>
+      <p className="fst-italic fw-light d-inline-block">{`Sunrise: ${sunrise} Sunset: ${sunset}`}</p>
+      <div className="row my-5">
+        <div className="col-6 d-flex align-items-center">
+          <img
+            src={`${weatherIconLink}${weatherIcon}.png`}
+            alt={weatherDescription}
+            width="150px"
+            className="text-center"
+          />
+        </div>
+        <div className="col-6 d-flex justify-content-center align-items-center">
+          <p className="temperature display-1 text-center">{`${temperature}\u00B0C`}</p>
+        </div>
+      </div>
+      <div>
+        <p className="text-start fst-italic fw-light">{`${weatherDescription}`}</p>
+        <div className="d-flex justify-content-between">
+          <p className="fst-italic fw-light d-inline-block">{`Windspeed: ${windSpd} m/s ${windDir}`}</p>
+          <p className="fst-italic fw-light d-inline-block">{`Rain: ${precip}mm`}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const StyledCurrentForecastMain = styled(CurrentForecastMain)`
+  .temperature {
+    font-size: 72px;
+  }
+`
+
+export default StyledCurrentForecastMain
+
+CurrentForecastMain.propTypes = {
+  currentWeatherData: PropTypes.object,
+  className: PropTypes.string,
+}
+
+CurrentForecastMain.defaultProps = {
+  currentWeatherData: {},
+  className: '',
+}
